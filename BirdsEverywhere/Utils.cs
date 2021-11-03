fc04053e6a71f0f18c3f199ab898c908c65779c3
pschonev <dasmotte@gmx.net>
2021-11-03 19:46:21 +0100

    simplified weighted shuffle

--- BirdsEverywhere/Utils.cs
+++ BirdsEverywhere/Utils.cs
@@ -2,27 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using System.Text;
-using System.Threading.Tasks;
 using StardewValley;
 
 namespace BirdsEverywhere
 {
     class Utils
     {
-        private static double transform(int p)
+        private static double transform(double w)
         {
-            return Math.Pow(Game1.random.NextDouble(), 1.0 / (double)p) * -1;
+            return (w / 2) + (Game1.random.NextDouble() / 2);
         }
 
         public static List<T> shuffleByWeights<T>(List<T> items, List<int> weights)
         {
             List<int> indexes = Enumerable.Range(0, items.Count).ToList();
-            indexes.Sort(delegate (int x, int y) {
-                return transform(weights[x]).CompareTo(transform(weights[y]));
-            });
+            int maxWeight = weights.Max();
+            List<double> zeroToOneWeights = weights.Select(x => (double)x / maxWeight).ToList();
+
+            indexes.Sort((x, y) =>
+                transform(weights[x]).CompareTo(transform(weights[y]))
+            );
             return indexes.Select(x => items[x]).ToList();
         }
+
+
         public static T getRandomElementFromList<T>(List<T> listToPickFrom)
         {
             int index = Game1.random.Next(0, Math.Max(listToPickFrom.Count - 1, 0));
