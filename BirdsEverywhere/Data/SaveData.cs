1821e758ab4244bc1e41d58ce145b104cd425eaa
pschonev <dasmotte@gmx.net>
2021-11-07 15:01:16 +0100

    saveData now only contains seenBirds

--- BirdsEverywhere/Data/SaveData.cs
+++ BirdsEverywhere/Data/SaveData.cs
@@ -5,7 +5,6 @@ namespace BirdsEverywhere
 {
     public class SaveData
     {
-        public List<string> unseenBirds { get; set; }
         public HashSet<string> seenBirds { get; set; } = new HashSet<string>();
 
     }
