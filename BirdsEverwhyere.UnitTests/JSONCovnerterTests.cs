fa53340ff993098fe54060a9052400c544834bc3
pschonev <p.schoneville@gmx.net>
2022-02-07 21:59:04 +0100

    added tests (not working so far)

---
+++ BirdsEverwhyere.UnitTests/JSONCovnerterTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using StardewValley;
+using Newtonsoft.Json;
+
+namespace BirdsEverwhyere.UnitTests
+{
+    public class JSONConverterTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void SendAnimatedSprite()
+        {
+            var sprite = new StardewValley.AnimatedSprite();
+            string jsonString = JsonConvert.SerializeObject(sprite);
+        }
+    }
+}
