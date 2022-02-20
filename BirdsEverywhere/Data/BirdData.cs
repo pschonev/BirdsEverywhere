f41760e184041ad183ca3fc07e6b2467974c7d6c
pschonev <p.schoneville@gmx.net>
2022-02-20 15:43:43 +0100

    fields to properties

--- BirdsEverywhere/Data/BirdData.cs
+++ BirdsEverywhere/Data/BirdData.cs
@@ -8,41 +8,43 @@ namespace BirdsEverywhere
 {
     public class BirdData
     {
-        public string id;
-        public string name;
-        public string scName;
-        public string description = "";
+        public string id { get; set; }
+        public string name { get; set; }
+        public string scName { get; set; }
+        public string description { get; set; } = "";
 
-        public string egg = "";
-        public string breedingSeason = "spring";
-        public string feather = "";
-        public bool dimorphism = false;
+        public string egg { get; set; } = "";
+        public string breedingSeason { get; set; } = "spring";
+        public string feather { get; set; } = "";
+        public bool dimorphism { get; set; } = false;
 
-        public string template = "";
+        public string template { get; set; } = "";
         public List<string> seasons = new List<string>() { "spring", "summer", "fall", "winter" };
 
-        public SpawnData spawnData = new SpawnData();
-        public Dictionary<string, List<SpawnData>> advancedSpawn = new Dictionary<string, List<SpawnData>>(); // advanced spawn patters in the form season : SpawnData
+        public SpawnData spawnData { get; set; } = new SpawnData();
+        public Dictionary<string, List<SpawnData>> advancedSpawn { get; set; } = new Dictionary<string, List<SpawnData>>(); // advanced spawn patters in the form season : SpawnData
+
+
     }
 
     public class SpawnData
     {
-        public List<string> locations = new List<string>() { "Backwoods" }; // possible spawn locations
-        public string spawnPattern = "SpawnableGroundSpawner"; // how to spawn the bird at location
-        public string birdType = "LandBird"; // Bird class derivative that determines behavior
-        public int minGroupCount = 1; //minimum amount of groups that are created
-        public int maxGroupCount = 2;
-        public int minGroupSize = 1; //minimum amount of birds in a group
-        public int maxGroupSize = 5;
-        public bool requireRain = false;
-        public List<timeRange> timeOfDayRanges = new List<timeRange>() { new timeRange(), new timeRange() }; // during which time of the day they can spawn
-        public List<timeRange> daysRanges = new List<timeRange>() { new timeRange(1, 30) }; // when in the season they can spawn
+        public List<string> locations { get; set; } = new List<string>() { "Backwoods" }; // possible spawn locations
+        public string spawnPattern { get; set; } = "SpawnableGroundSpawner"; // how to spawn the bird at location
+        public string birdType { get; set; } = "LandBird"; // Bird class derivative that determines behavior
+        public int minGroupCount { get; set; } = 1; //minimum amount of groups that are created
+        public int maxGroupCount { get; set; } = 2;
+        public int minGroupSize { get; set; } = 1; //minimum amount of birds in a group
+        public int maxGroupSize { get; set; } = 5;
+        public bool requireRain { get; set; } = false;
+        public List<timeRange> timeOfDayRanges { get; set; } = new List<timeRange>() { new timeRange(), new timeRange() }; // during which time of the day they can spawn
+        public List<timeRange> daysRanges { get; set; } = new List<timeRange>() { new timeRange(1, 30) }; // when in the season they can spawn
     }
 
     public class timeRange
     {
-        public int minTime = 600;
-        public int maxTime = 1800;
+        public int minTime { get; set; } = 600;
+        public int maxTime { get; set; } = 1800;
 
         public timeRange(){}
 
