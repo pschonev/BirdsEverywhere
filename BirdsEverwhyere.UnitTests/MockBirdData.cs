c51bacb8eff435a1d1f0cf8866a7af793cefbf69
pschonev <p.schoneville@gmx.net>
2022-03-05 22:37:19 +0100

    added mock template class

--- BirdsEverwhyere.UnitTests/MockBirdData.cs
+++ BirdsEverwhyere.UnitTests/MockBirdData.cs
@@ -9,7 +9,7 @@ namespace BirdsEverwhyere.UnitTests
 {
     class MockBirdData : BirdData
     {
-        private static string projectDir =
+        public static string projectDir =
     Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\.."));
 
         [JsonConstructor]
@@ -20,7 +20,7 @@ namespace BirdsEverwhyere.UnitTests
             {
                 string templatePath = Path.Combine(projectDir, $"{template}.json");
                 string templateJson = File.ReadAllText(templatePath);
-                MockBirdData templateData = JsonSerializer.Deserialize<MockBirdData>(templateJson);
+                MockBirdTemplate templateData = JsonSerializer.Deserialize<MockBirdTemplate>(templateJson);
                 templateData.defaultSpawnData.CopyProperties(defaultSpawnData);
                 allSpawnData.MergeDictionaries(templateData.allSpawnData);
             }
@@ -34,8 +34,37 @@ namespace BirdsEverwhyere.UnitTests
             }
 
             this.defaultSpawnData = defaultSpawnData;
-            this.allSpawnData = insertDefaults(this.defaultSpawnData, allSpawnData);
-            this.allSpawnData = insertDefaults(globalDefaultSpawndata, this.allSpawnData);
+            this.allSpawnData = SpawnData.insertDefaults(this.defaultSpawnData, allSpawnData);
+            this.allSpawnData = SpawnData.insertDefaults(globalDefaultSpawndata, this.allSpawnData);
+        }
+    }
+
+    class MockBirdTemplate : BirdTemplate
+    {
+        [JsonConstructor]
+        public MockBirdTemplate(SpawnData defaultSpawnData, Dictionary<string, List<SpawnData>> allSpawnData, string template)
+           : base()
+        {
+            if (!string.IsNullOrEmpty(template))
+            {
+                string templatePath = Path.Combine(MockBirdData.projectDir, $"{template}.json");
+                string templateJson = File.ReadAllText(templatePath);
+                MockBirdTemplate templateData = JsonSerializer.Deserialize<MockBirdTemplate>(templateJson);
+                templateData.defaultSpawnData.CopyProperties(defaultSpawnData);
+                allSpawnData.MergeDictionaries(templateData.allSpawnData);
+            }
+            if (allSpawnData == null)
+            {
+                allSpawnData = new Dictionary<string, List<SpawnData>>();
+            }
+            if (defaultSpawnData == null)
+            {
+                defaultSpawnData = SpawnData.getDefaultSpawnData();
+            }
+
+            this.defaultSpawnData = defaultSpawnData;
+            this.allSpawnData = SpawnData.insertDefaults(this.defaultSpawnData, allSpawnData);
+            this.allSpawnData = SpawnData.insertDefaults(globalDefaultSpawndata, this.allSpawnData);
         }
     }
 }
